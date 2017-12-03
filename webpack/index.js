30788d3511c1319d1e16288f0e989201215f6862
surunzi <surunzi@foxmail.com>
2017-12-03 15:34:00 +0800

    Dev: Webpack template

--- webpack/index.js
+++ webpack/index.js
@@ -1,8 +1,23 @@
 export default function (eruda) 
 {
     class Plugin extends eruda.Tool {
-
+        init($el, container) 
+        {
+            super.init($el, container);
+        }
+        show() 
+        {
+            super.show();
+        }
+        hide()
+        {
+            super.hide();
+        }
+        destroy() 
+        {
+            super.destroy();
+        }
     };
 
-    return new Plugin;
+    return new Plugin();
 };
