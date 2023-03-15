95dd90fbdac1929099b73265003e6f0c7066da11
redhoodsu <redhoodsu@tencent.com>
2023-03-15 20:03:30 +0800

    chore: small changes

--- index.js
+++ index.js
@@ -1,84 +1,84 @@
-const inquirer = require('inquirer');
-const path = require('path');
-const fs = require('fs-extra');
-const glob = require('glob');
+const inquirer = require('inquirer')
+const path = require('path')
+const fs = require('fs-extra')
+const glob = require('glob')
 
-const util = require('./util');
+const util = require('./util')
 
-const prompt = async questions => inquirer.prompt(questions);
-const ensureDir = async dir => fs.ensureDir(dir);
-const readdir = async dir => {
-    return new Promise((resolve, reject) => {
-        glob(dir, {}, (err, data) => {
-            if (err) return reject(err);
+const prompt = async (questions) => inquirer.prompt(questions)
+const ensureDir = async (dir) => fs.ensureDir(dir)
+const readdir = async (dir) => {
+  return new Promise((resolve, reject) => {
+    glob(dir, {}, (err, data) => {
+      if (err) return reject(err)
 
-            resolve(data);
-        });
-    });
-};
-const readFile = async path => fs.readFile(path, 'utf-8');
-const writeFile = async (path, data) => fs.writeFile(path, data, 'utf-8');
-(async () => {
-    let answers = await prompt([
-        {
-            type: 'list',
-            name: 'type',
-            message: 'Which template?',
-            choices: ['simple', 'webpack']
-        },
-        {
-            name: 'name',
-            message: 'Plugin name:'
-        }
-    ]);
+      resolve(data)
+    })
+  })
+}
+const readFile = async (path) => fs.readFile(path, 'utf-8')
+const writeFile = async (path, data) => fs.writeFile(path, data, 'utf-8')
+;(async () => {
+  let answers = await prompt([
+    {
+      type: 'list',
+      name: 'type',
+      message: 'Which template?',
+      choices: ['simple', 'webpack'],
+    },
+    {
+      name: 'name',
+      message: 'Plugin name:',
+    },
+  ])
 
-    let { type, name } = answers;
+  let { type, name } = answers
 
-    if (name.trim() === '') return console.log('Please enter a plugin name!');
-    name = name.toLowerCase();
+  if (name.trim() === '') return console.log('Please enter a plugin name!')
+  name = name.toLowerCase()
 
-    let dist = path.resolve(process.cwd(), `./eruda-${name}`),
-        src = path.resolve(__dirname, `./${type}`);
+  let dist = path.resolve(process.cwd(), `./eruda-${name}`),
+    src = path.resolve(__dirname, `./${type}`)
 
-    await ensureDir(dist);
+  await ensureDir(dist)
 
-    let ignoreList = await readFile(path.resolve(src, './plugin.gitignore'));
-    ignoreList = ignoreList
-        .split(/\n/g)
-        .map(p => util.trim(p.trim(), '/').replace(/\//g, path.sep))
-        .filter(p => p !== '' && p !== 'node_modules');
+  let ignoreList = await readFile(path.resolve(src, './plugin.gitignore'))
+  ignoreList = ignoreList
+    .split(/\n/g)
+    .map((p) => util.trim(p.trim(), '/').replace(/\//g, path.sep))
+    .filter((p) => p !== '' && p !== 'node_modules')
 
-    let files = util.concat(
-        await readdir(src + '/**/*.*'),
-        await readdir(src + '/.*')
-    );
+  let files = util.concat(
+    await readdir(src + '/**/*.*'),
+    await readdir(src + '/.*')
+  )
 
-    await files.forEach(async file => {
-        file = path.normalize(file).replace(src + path.sep, '');
+  await files.forEach(async (file) => {
+    file = path.normalize(file).replace(src + path.sep, '')
 
-        let srcPath = path.resolve(src, file);
+    let srcPath = path.resolve(src, file)
 
-        // Make sure gitignore and npmignore aren't taking effect when published.
-        file = file
-            .replace('plugin.gitignore', '.gitignore')
-            .replace('plugin.npmignore', '.npmignore');
-        let distPath = path.resolve(dist, file.replace('plugin', name));
+    // Make sure gitignore and npmignore aren't taking effect when published.
+    file = file
+      .replace('plugin.gitignore', '.gitignore')
+      .replace('plugin.npmignore', '.npmignore')
+    let distPath = path.resolve(dist, file.replace('plugin', name))
 
-        for (let ignorePath of ignoreList) {
-            if (srcPath.indexOf(ignorePath) > -1) return;
-        }
+    for (let ignorePath of ignoreList) {
+      if (srcPath.indexOf(ignorePath) > -1) return
+    }
 
-        let data = await readFile(srcPath);
-        data = data
-            .replace(/(eruda-)plugin/gi, `$1${name}`)
-            .replace(/\bPlugin\b/g, util.upperFirst(name))
-            .replace(/erudaPlugin/g, `eruda${util.upperFirst(name)}`)
-            .replace(/'plugin'/g, `'${name}'`)
-            .replace(/\.plugin {/g, `.${name} {`);
+    let data = await readFile(srcPath)
+    data = data
+      .replace(/(eruda-)plugin/gi, `$1${name}`)
+      .replace(/\bPlugin\b/g, util.upperFirst(name))
+      .replace(/erudaPlugin/g, `eruda${util.upperFirst(name)}`)
+      .replace(/'plugin'/g, `'${name}'`)
+      .replace(/\.plugin {/g, `.${name} {`)
 
-        await ensureDir(path.dirname(distPath));
-        await writeFile(distPath, data);
-    });
+    await ensureDir(path.dirname(distPath))
+    await writeFile(distPath, data)
+  })
 
-    console.log(`cd eruda-${name} && npm i;`);
-})();
+  console.log(`cd eruda-${name} && npm i;`)
+})()
--- simple/eruda-plugin.js
+++ simple/eruda-plugin.js
@@ -1,42 +1,40 @@
-(function(root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
-    } else {
-        root.erudaPlugin = factory();
-    }
-})(this, function() {
-    return function(eruda) {
-        var Tool = eruda.Tool,
-            util = eruda.util;
+;(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory)
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory()
+  } else {
+    root.erudaPlugin = factory()
+  }
+})(this, function () {
+  return function (eruda) {
+    var Tool = eruda.Tool,
+      util = eruda.util
 
-        var Plugin = Tool.extend({
-            name: 'plugin',
-            init: function($el) {
-                this.callSuper(Tool, 'init', arguments);
-                this._style = util.evalCss(
-                    [
-                        '.eruda-dev-tools .eruda-tools .eruda-plugin {padding: 10px;}',
-                        '.eruda-tip {padding: 10px; background: #fff; color: #263238;}'
-                    ].join('.eruda-dev-tools .eruda-tools .eruda-plugin ')
-                );
-                $el.html(
-                    '<div class="eruda-tip">Put whatever you want here:)</div>'
-                );
-            },
-            show: function() {
-                this.callSuper(Tool, 'show', arguments);
-            },
-            hide: function() {
-                this.callSuper(Tool, 'hide', arguments);
-            },
-            destroy: function() {
-                this.callSuper(Tool, 'destroy', arguments);
-                util.evalCss.remove(this._style);
-            }
-        });
+    var Plugin = Tool.extend({
+      name: 'plugin',
+      init: function ($el) {
+        this.callSuper(Tool, 'init', arguments)
+        this._style = util.evalCss(
+          [
+            '.eruda-dev-tools .eruda-tools .eruda-plugin {padding: 10px;}',
+            '.eruda-tip {padding: 10px; background: #fff; color: #263238;}',
+          ].join('.eruda-dev-tools .eruda-tools .eruda-plugin ')
+        )
+        $el.html('<div class="eruda-tip">Put whatever you want here:)</div>')
+      },
+      show: function () {
+        this.callSuper(Tool, 'show', arguments)
+      },
+      hide: function () {
+        this.callSuper(Tool, 'hide', arguments)
+      },
+      destroy: function () {
+        this.callSuper(Tool, 'destroy', arguments)
+        util.evalCss.remove(this._style)
+      },
+    })
 
-        return new Plugin();
-    };
-});
+    return new Plugin()
+  }
+})
